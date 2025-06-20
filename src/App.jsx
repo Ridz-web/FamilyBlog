a0c825684b21d3a4f0c089be8296f8890027dc62
Ridz-web <mutaalibudi@gmail.com>
2025-06-20 10:18:55 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./index.css";
 import Home from "./Home";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Router basename='/FamilyBlog'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
